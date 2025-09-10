959f06aeb3af9c3d0e5c1b4db5eccc0dabef3301
zahabiski <zakharii.tatarchuk@gmail.com>
2025-09-10 17:52:16 +0100

    Update profile.py

--- pages/profile.py
+++ pages/profile.py
@@ -1,3 +1,25 @@
-import streamlit as st 
+# ---------------------- IMPORTS ----------------------
 
-st.write("second page")
+import streamlit as st
+from PIL import Image
+import time
+
+# ---------------------- PAGE CONFIG ----------------------
+
+if "page" not in st.session_state:
+    st.session_state.page = "quiz"
+    
+im = Image.open("logo-round.png")
+
+st.set_page_config(
+    page_title="Find Yourself",
+    page_icon=im,
+    layout="centered"
+)
+
+# ---------------------- PAGE HEADER ----------------------
+
+st.markdown(
+    "<h1 style='text-align: center; color: black;'>Find Yourself Quiz</h1>",
+    unsafe_allow_html=True
+)
