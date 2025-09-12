b47d0bc4ad0d4117c169366db757cd78b5cb8d27
zahabiski <zakharii.tatarchuk@gmail.com>
2025-09-12 18:35:49 +0100

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -26,120 +26,18 @@ st.markdown(
 # ---------------------- QUIZ DATA ----------------------
 
 quiz = [
-    {
-        "question": " What is your Gender?",
-        "options": ["Male", "Female"]
-    },
-    {
-        "question": " What is more related to you?",
-        "options": [
-            "work with nature",
-            "work with people",
-            "work with technics",
-            "work with art",
-            "work with symbols"
-        ]
-    },
-    {
-        "question": " Do you feel comfortable working in team?",
-        "options": [
-            "Yes, I love communication",
-            "Mostly, but sometimes alone",
-            "Sometimes, It depends",
-            "Not Really, but would like to get some help",
-            "No, I’d rather do it alone"
-        ]
-    },
-    {
-        "question": " Do you enjoy solving mathematical and computer problems?",
-        "options": [
-            "Yes, I do",
-            "Mostly, but not good with computers",
-            "Sometimes, It depends",
-            "Not Really, but like technologies",
-            "No, it’s not for me"
-        ]
-    },
-    {
-        "question": " Are you comfortable adapting quickly when things don't go as planned?",
-        "options": [
-            "Yes, I adapt fast and calm",
-            "Mostly, but stress affects me",
-            "Sometimes, It depends",
-            "Not Really, but I good manage stress",
-            "No, I prefer stable and predictable situation"
-        ]
-    },
-    {
-        "question": " Are you interested in learning how the human body and nature works?",
-        "options": [
-            "Yes, It's really interesting",
-            "Mostly, but prefer help people",
-            "Sometimes, It depends",
-            "Not Really, but I'm a bit afraid of medical tools",
-            "No, It's not mine"
-        ]
-    },
-    {
-        "question": " Do you usually understand what others feel and how to respond emotionally?",
-        "options": [
-            "Yes, I'm very sensitive",
-            "Mostly, but hard to understand other",
-            "Sometimes, It depends",
-            "Not Really, but I understand people well",
-            "No, I'm kinda robot"
-        ]
-    },
-    {
-        "question": " Do you enjoy managing finances and running projects?",
-        "options": [
-            "Yes, It's really interests me",
-            "Mostly, but I'm not good in math",
-            "Sometimes, It depends",
-            "Not Really, but I'm a good mathematician",
-            "No, It's all hard for me"
-        ]
-    },
-    {
-        "question": " Do you often come up with new ideas and enjoy experimenting with them?",
-        "options": [
-            "Yes, I'm very creative",
-            "Mostly, but not with new things",
-            "Sometimes, It depends",
-            "Not Really, but new stuff is mine passion",
-            "No, I'm not that open-minded"
-        ]
-    },
-    {
-        "question": " Do you enjoy learning languages, exploring culture and history?",
-        "options": [
-            "Yes, It's very interesting",
-            "Mostly, but I prefer learning languages only",
-            "Sometimes, It depends",
-            "Not Really, but I like exploring culture",
-            "No, I don't enjoy writing or the past"
-        ]
-    },
-    {
-        "question": " Do you enjoy leading people and organizing processes?",
-        "options": [
-            "Yes, I love lead and being responsible",
-            "Mostly, but I'm bad at managing tasks",
-            "Sometimes, It depends",
-            "Not Really, but I can manage tasks well",
-            "No, I’d rather be a part of the machine"
-        ]
-    },
-    {
-        "question": " Do you like working with visuals, sounds and building artistic things?",
-        "options": [
-            "Yes, I’m pretty creative in these areas",
-            "Mostly, but hard in realization",
-            "Sometimes, It depends",
-            "Not Really, but I can bring others' ideas to life",
-            "No, that’s absolutely not me"
-        ]
-    }
+    {"question": " What is your Gender?", "options": ["Male", "Female"]},
+    {"question": " What is more related to you?", "options": ["work with nature","work with people","work with technics","work with art","work with symbols"]},
+    {"question": " Do you feel comfortable working in team?", "options": ["Yes, I love communication","Mostly, but sometimes alone","Sometimes, It depends","Not Really, but would like to get some help","No, I’d rather do it alone"]},
+    {"question": " Do you enjoy solving mathematical and computer problems?", "options": ["Yes, I do","Mostly, but not good with computers","Sometimes, It depends","Not Really, but like technologies","No, it’s not for me"]},
+    {"question": " Are you comfortable adapting quickly when things don't go as planned?", "options": ["Yes, I adapt fast and calm","Mostly, but stress affects me","Sometimes, It depends","Not Really, but I good manage stress","No, I prefer stable and predictable situation"]},
+    {"question": " Are you interested in learning how the human body and nature works?", "options": ["Yes, It's really interesting","Mostly, but prefer help people","Sometimes, It depends","Not Really, but I'm a bit afraid of medical tools","No, It's not mine"]},
+    {"question": " Do you usually understand what others feel and how to respond emotionally?", "options": ["Yes, I'm very sensitive","Mostly, but hard to understand other","Sometimes, It depends","Not Really, but I understand people well","No, I'm kinda robot"]},
+    {"question": " Do you enjoy managing finances and running projects?", "options": ["Yes, It's really interests me","Mostly, but I'm not good in math","Sometimes, It depends","Not Really, but I'm a good mathematician","No, It's all hard for me"]},
+    {"question": " Do you often come up with new ideas and enjoy experimenting with them?", "options": ["Yes, I'm very creative","Mostly, but not with new things","Sometimes, It depends","Not Really, but new stuff is mine passion","No, I'm not that open-minded"]},
+    {"question": " Do you enjoy learning languages, exploring culture and history?", "options": ["Yes, It's very interesting","Mostly, but I prefer learning languages only","Sometimes, It depends","Not Really, but I like exploring culture","No, I don't enjoy writing or the past"]},
+    {"question": " Do you enjoy leading people and organizing processes?", "options": ["Yes, I love lead and being responsible","Mostly, but I'm bad at managing tasks","Sometimes, It depends","Not Really, but I can manage tasks well","No, I’d rather be a part of the machine"]},
+    {"question": " Do you like working with visuals, sounds and building artistic things?", "options": ["Yes, I’m pretty creative in these areas","Mostly, but hard in realization","Sometimes, It depends","Not Really, but I can bring others' ideas to life","No, that’s absolutely not me"]}
 ]
 
 # ---------------------- QUIZ LOGIC ----------------------
@@ -270,6 +168,7 @@ footer {visibility: hidden;}
 
 </style>
 """, unsafe_allow_html=True)
+
 
 
 
